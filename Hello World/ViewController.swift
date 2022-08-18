7448385dd8f6ae929350ede76c99c335617494e7
yahiabeethoven <ybt214@nyu.edu>
2022-08-18 19:13:48 +0200

    bugs

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func ScreenClicked(_ sender: Any) {
+        self.view.backgroundColor = UIColor.tintColor
+        TextLabel.textColor = UIColor.tintColor
+        TextLabel.text = "Goodbye 👋"
+    }
+    func getRandomColor() -> UIColor {
+         let red:CGFloat = CGFloat(drand48())
+         let green:CGFloat = CGFloat(drand48())
+         let blue:CGFloat = CGFloat(drand48())
 
+         return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
+    }
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        TextLabel.textColor = getRandomColor()
+        TextLabel.text = "Hello World Prework"
+    }
+    
+    @IBAction func Button2Clicked(_ sender: Any) {
+        self.view.backgroundColor = getRandomColor()
+        TextLabel.text = "Hello World Prework"
+    }
 }
 
